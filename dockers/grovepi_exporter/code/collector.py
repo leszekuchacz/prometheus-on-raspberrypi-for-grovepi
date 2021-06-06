c133c88de0fbabc09a4b7b57a04f4ba72da4187e
Leszek Uchacz <bloodzio@gmail.com>
2021-06-06 14:43:28 +0100

    Add default rules for all sensors #10

--- dockers/grovepi_exporter/code/collector.py
+++ dockers/grovepi_exporter/code/collector.py
@@ -1,8 +1,8 @@
 import time, os, grovepi, math, smbus2, bme280
-from prometheus_client.core import GaugeMetricFamily, REGISTRY, CounterMetricFamily
-from prometheus_client import start_http_server
+from prometheus_client.core import GaugeMetricFamily, REGISTRY, CounterMetricFamily, InfoMetricFamily
+from prometheus_client import start_http_server, Info
 
-
+# grovepi envs
 grovepi_exporter_port=int(os.environ.get('grovepi_exporter_port','8081'))
 grovepi_dht_dport=int(os.environ.get('grovepi_dht_dport','-1'))
 grovepi_dht_dport=int(os.environ.get('grovepi_dht_dport','-1'))
@@ -12,6 +12,24 @@ grovepi_hcho_r0=int(os.environ.get('grovepi_hcho_r0','25'))
 grovepi_sound_aport=int(os.environ.get('grovepi_sound_aport','-1'))
 grovepi_light_aport=int(os.environ.get('grovepi_light_aport','-1'))
 grovepi_barometer_type=str(os.environ.get('grovepi_barometer_type',''))
+
+# alerts envs
+alert_temperature_max_warning=int(os.environ.get('alert_temperature_max_warning','26'))
+alert_temperature_max_firing=int(os.environ.get('alert_temperature_max_firing','32'))
+alert_temperature_min_warning=int(os.environ.get('alert_temperature_min_warning','18'))
+alert_temperature_min_firing=int(os.environ.get('alert_temperature_min_firing','5'))
+alert_humidity_max_warning=int(os.environ.get('alert_humidity_max_warning','80'))
+alert_humidity_max_firing=int(os.environ.get('alert_humidity_max_firing','90'))
+alert_humidity_min_warning=int(os.environ.get('alert_humidity_min_warning','20'))
+alert_humidity_min_firing=int(os.environ.get('alert_humidity_min_firing','10'))
+alert_pressure_max_warning=int(os.environ.get('alert_pressure_max_warning','1020'))
+alert_pressure_max_firing=int(os.environ.get('alert_pressure_max_firing','1050'))
+alert_pressure_min_warning=int(os.environ.get('alert_pressure_min_warning','980'))
+alert_pressure_min_firing=int(os.environ.get('alert_pressure_min_firing','950'))
+alert_ppm_max_warning=int(os.environ.get('alert_ppm_max_warning','4'))
+alert_ppm_max_firing=int(os.environ.get('alert_ppm_max_firing','10'))
+alert_loudless_max_warning=int(os.environ.get('alert_loudless_max_warning','400'))
+alert_loudless_max_firing=int(os.environ.get('alert_loudless_max_firing','600'))
 
 class CustomCollector(object):
   def __init__(self):
@@ -48,7 +66,31 @@ class CustomCollector(object):
       yield GaugeMetricFamily("grovepi_barometer_temperature", '°C', value=data.temperature)
       yield GaugeMetricFamily("grovepi_barometer_humidity", 'rH', value=data.humidity)
       yield GaugeMetricFamily("grovepi_barometer_pressure", 'hPa', value=data.pressure)
-       
+    # Info - tresholds for alerts in prometheus
+    yield GaugeMetricFamily("alert_temperature_max_warning","",value=alert_temperature_max_warning)    
+    yield GaugeMetricFamily("alert_temperature_max_firing", "",value=alert_temperature_max_firing)    
+    yield GaugeMetricFamily("alert_temperature_min_warning","",value=alert_temperature_min_warning)    
+    yield GaugeMetricFamily("alert_temperature_min_firing", "",value=alert_temperature_min_firing)    
+    yield GaugeMetricFamily("alert_humidity_max_warning","",value=alert_humidity_max_warning)    
+    yield GaugeMetricFamily("alert_humidity_max_firing", "",value=alert_humidity_max_firing)    
+    yield GaugeMetricFamily("alert_humidity_min_warning","",value=alert_humidity_min_warning)    
+    yield GaugeMetricFamily("alert_humidity_min_firing", "",value=alert_humidity_min_firing)    
+    yield GaugeMetricFamily("alert_pressure_max_warning","",value=alert_pressure_max_warning)    
+    yield GaugeMetricFamily("alert_pressure_max_firing", "",value=alert_pressure_max_firing)    
+    yield GaugeMetricFamily("alert_pressure_min_warning","",value=alert_pressure_min_warning)    
+    yield GaugeMetricFamily("alert_pressure_min_firing", "",value=alert_pressure_min_firing)    
+    yield GaugeMetricFamily("alert_ppm_max_warning", "",value=alert_ppm_max_warning)    
+    yield GaugeMetricFamily("alert_ppm_max_firing", "",value=alert_ppm_max_firing)    
+    yield GaugeMetricFamily("alert_loudless_max_warning", "",value=alert_loudless_max_warning)    
+    yield GaugeMetricFamily("alert_loudless_max_firing", "",value=alert_loudless_max_firing)    
+    
+    
+    
+    # Can't export label as value, info have always value 1
+    #i = InfoMetricFamily("grovie_vars", "T")
+    #i.add_metric(labels="sensor", value={ "type": "temperature","name": "max", "severity": "warning", "value": "27"})
+    #i.add_metric(labels="sensor", value={"type": "temperature","name": "max", "severity": "firing", "value": "30"})
+    #yield i 
       
       
 if __name__ == '__main__':
